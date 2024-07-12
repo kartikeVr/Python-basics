35440751a85cdb6c887f2fcd5c8813b3178a8a31
Kartike verma <117355375+kartikeVr@users.noreply.github.com>
2024-07-12 12:46:10 +0530

    Add files via upload

---
+++ Table-creation.py
@@ -0,0 +1,14 @@
+import sqlite3
+conn = sqlite3.connect("sqlite.db")
+ch = f"""CREATE TABLE IF NOT EXISTS store (
+                        Name VARCHAR(35),
+                        Course VARCHAR(35),
+                        Batch VARCHAR(35),
+                        Enrollment_No VARCHAR(25) PRIMARY KEY,
+                        Count INTEGER DEFAULT 1,
+                        Time VARCHAR(35)
+);"""
+conn.execute(ch)
+
+conn.commit()
+conn.close()
